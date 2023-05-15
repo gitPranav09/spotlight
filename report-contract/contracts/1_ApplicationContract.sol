db1146fc05e6a47c278031224e75f57492f313c3
jhamadhav <jhamadhav28@gmail.com>
2023-05-15 20:10:22 +0530

    solidity contract made deployed to local ganache

---
+++ report-contract/contracts/1_ApplicationContract.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+contract ApplicationContract {
+    struct Application {
+        string id;
+        string subject;
+        string description;
+        string file;
+        address applierAddress;
+        uint256 votes;
+    }
+
+    mapping(string => Application) public applications;
+    mapping(address => string[]) public applicationsMadeByAddress;
+    mapping(address => uint256) public rewardBank;
+    string[] allApplications;
+
+    function create(
+        string memory id,
+        string memory subject,
+        string memory description,
+        string memory file
+    ) public {
+        Application memory a;
+        a.id = id;
+        a.subject = subject;
+        a.description = description;
+        a.applierAddress = msg.sender;
+        a.file = file;
+        a.votes = 0;
+        
+        applications[id] = a;
+        applicationsMadeByAddress[msg.sender].push(id);
+
+        allApplications.push(id);
+    }
+
+
+    // function to view appication
+    function getApplication(string memory id) public view returns (Application memory) {
+        return applications[id];
+    }
+
+    function getApplicationsMadeByAddress() public view returns(string[] memory){
+        return applicationsMadeByAddress[msg.sender];
+    }
+
+    function getAllApplicationsID() public view returns(string[] memory){
+        return allApplications;
+    }
+
+
+    // reward system
+    function addUpvote(string memory id, uint256 amount) payable public{
+        require(msg.value == amount);
+        require(amount <= address(msg.sender).balance);
+
+
+        rewardBank[ applications[id].applierAddress ] += amount;
+
+        Application memory a = applications[id];
+        a.votes += 1;
+        applications[a.id] = a;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return rewardBank[msg.sender];
+    }
+
+    function withdraw() payable public {
+        address payable user = payable(msg.sender);
+        user.transfer( rewardBank[msg.sender] );
+    }
+
+    // test function to see your own ID
+    function getID() public view returns(address){
+        return msg.sender;
+    }
+
+}
---
+++ report-contract/truffle-config.js
@@ -0,0 +1,33 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+module.exports = {
+  contracts_build_directory: "./",
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    // matic: {
+    //   provider: () =>
+    //     new HDWalletProvider(mnemonic, `https://polygon-mumbai.g.alchemy.com/v2/5tsUoxKQI8SDorSD9o3_XtZA_SF4khsT`),
+    //   network_id: 80001,
+    //   confirmations: 2,
+    //   timeoutBlocks: 200,
+    //   skipDryRun: true,
+    // },
+
+  },
+
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.17",      // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  },
+};
